92409ef1e7aab951cee4b2788891f7b76329d0f9
sclayturner <cturner@turnerlegacy.com>
2016-10-01 13:27:49 -0400

    Lots of updates here. Reworked the solution. It now has all .NETCoreApp assemblies, which is the go-forward framework. Added an ASP.NET MVC app and cofigured it for EntityFramework for Core and SQL Server adapter.  I also added two other assemblies where we will abstract away the domain models, and configure them.  These two are .NET Core App assemblies w/ EF Core.  Lastly, there is a common assembly. TODO: Need to setup references. Figure out code-first migrations with Core.  The FitnessFrog.Core.UI when run with IIS Express provides documenation to everything we need to flesh out, including code-first migrations.

---
+++ FitnessFrog/FitnessFrog.Core.Domain.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FitnessFrog.Core.Domain.Data")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c5b5b412-8c20-4b6b-8d35-5fa9c3b47688")]
