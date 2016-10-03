f7b8c7b3e4eb1cc043f032f497403a71696255b5
Chris <Chris@CHRIS-PC>
2016-10-03 14:03:15 -0400

    Added classes for baseline project requirements

---
+++ FitnessFrog/FitnessFrog.Core/Food.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessFrog.Core
+{
+    public class Food
+    {
+        public int FoodId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Fats { get; set; }
+
+        public int Carbs { get; set; }
+
+        public int Proteins { get; set; }
+
+        public int Calories { get; set; }
+        /// <summary>
+        /// Gets or sets the Users notes of Food if needed.
+        /// e.g. liked/disliked the food.
+        /// </summary>
+        public string Note { get; set; }
+    }
+}
