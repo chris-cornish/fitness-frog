0d224623b858fdcdef0b1887a8345425f2980fdd
chris-cornish <csquare2390@gmail.com>
2016-10-05 15:16:07 -0400

    Updated FoodEntityConfig. #7

--- FitnessFrog/FitnessFrog.Core.Domain.Data/FoodEntityConfig.cs
+++ FitnessFrog/FitnessFrog.Core.Domain.Data/FoodEntityConfig.cs
@@ -49,6 +49,56 @@ namespace FitnessFrog.Core.Domain.Data
 
 
         #region "private helper methods"
+
+        private void Initialize(ref EntityTypeBuilder<Food> entityBuilder)
+        {
+
+            // Define the table name and schema apart from default conventions.
+            entityBuilder.ToTable(TableName, "FitnessFrog");
+
+            // Define primary key apart from default conventions.
+            entityBuilder.HasKey(x => x.UserId);
+
+            // Define relationships and scope on appropriate key(s) apart from default conventions.
+            entityBuilder.HasMany<Note>(o => o.Notes).WithOne().HasForeignKey(o => o.UserId);
+
+            // Define data column names and constraints map to properties apart from default conventions.
+            entityBuilder.Property(a => a.UserId)
+                .HasColumnName("UserId")
+                .IsRequired();
+
+            entityBuilder.Property(a => a.FoodId)
+                .HasColumnName("FoodId")
+                .IsRequired();
+
+            entityBuilder.Property(a => a.Name)
+                .HasColumnName("Name")
+                .HasMaxLength(64)
+                .IsRequired();
+
+            entityBuilder.Property(a => a.Fats)
+                .HasColumnName("Fats");
+
+            entityBuilder.Property(a => a.Carbs)
+                .HasColumnName("Carbs");
+
+            entityBuilder.Property(a => a.Proteins)
+                .HasColumnName("Proteins");
+
+            entityBuilder.Property(a => a.Calories)
+                .HasColumnName("Calories");
+
+            entityBuilder.Property(a => a.CreationDate)
+                .HasColumnName("CreationDate")
+                .HasDefaultValue(DateTime.Now);
+
+            entityBuilder.Property(a => a.ModificationDate)
+                .HasColumnName("ModificationDate")
+                .HasDefaultValue(DateTime.Now);
+
+        }
+
+
         #endregion
 
     }
